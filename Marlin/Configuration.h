be963b78a2f62d70820d2dcdd82dd3dec7654396
Julian Rendell <juliangrendell@gmail.com>
2014-09-01 13:20:27 -0700

    removing hack for g29. Didn't work. There must be another trigger that breaks g29 sometimes.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -449,7 +449,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
   // X:9.80 Y:-44.40 Z:1.70
   #define X_PROBE_OFFSET_FROM_EXTRUDER -9.7
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 44.4
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -1.90
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -2.3
 
   #define Z_RAISE_BEFORE_HOMING 5    // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                       // Be sure you have this distance over your Z_MAX_POS in case
