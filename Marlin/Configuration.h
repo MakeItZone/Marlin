dd603917c3b3729ac3fd8d43520ddb736880751b
Julian Rendell <juliangrendell@gmail.com>
2020-02-08 01:25:29 -0800

    First cut at tuned PID values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -468,7 +468,7 @@
 
 // Comment the following line to disable PID and enable bang-bang.
 #define PIDTEMP
-#define BANG_MAX 220     // Limits current to nozzle while in bang-bang mode; 255=full current
+#define BANG_MAX 128     // Limits current to nozzle while in bang-bang mode; 255=full current
 #define PID_MAX BANG_MAX // Limits current to nozzle while PID is active (see PID_FUNCTIONAL_RANGE below); 255=full current
 #define PID_K1 0.95      // Smoothing factor within any PID loop
 #if ENABLED(PIDTEMP)
@@ -479,14 +479,14 @@
   //#define SLOW_PWM_HEATERS      // PWM with very low frequency (roughly 0.125Hz=8s) and minimum state time of approximately 1s useful for heaters driven by a relay
   //#define PID_PARAMS_PER_HOTEND // Uses separate PID parameters for each extruder (useful for mismatched extruders)
                                   // Set/get with gcode: M301 E[extruder number, 0-2]
-  #define PID_FUNCTIONAL_RANGE 10 // If the temperature difference between the target temperature and the actual temperature
+  #define PID_FUNCTIONAL_RANGE 30 // If the temperature difference between the target temperature and the actual temperature
                                   // is more than PID_FUNCTIONAL_RANGE then the PID will be shut off and the heater will be set to min/max.
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Buda 2.0 on 24V
-  #define DEFAULT_Kp 6
-  #define DEFAULT_Ki .3
-  #define DEFAULT_Kd 125
+  //#define DEFAULT_Kp 6
+  //#define DEFAULT_Ki .3
+  //#define DEFAULT_Kd 125
 
   // Buda 2.0 on 12V
   //#define DEFAULT_Kp 22.2
@@ -517,6 +517,12 @@
 //    #define  DEFAULT_Kp 26.29
 //    #define  DEFAULT_Ki 2.99
 //    #define  DEFAULT_Kd 57.72
+
+// JR Buda 2 PID AutoTune - with manual fiddling as it keeps overflowing
+#define DEFAULT_Kp 11.98
+#define DEFAULT_Ki 0.66
+#define DEFAULT_Kd 54.24
+
 
 #endif // PIDTEMP
 
@@ -579,9 +585,10 @@
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
   //24V 360W silicone heater from NPH on 3mm borosilicate (TAZ 2.2+)
   //As measured on Taz 3
-  #define  DEFAULT_bedKp 128.15
-  #define  DEFAULT_bedKi 12.76
-  #define  DEFAULT_bedKd 321.85
+  //BED
+  #define DEFAULT_bedKp 41.25
+  #define DEFAULT_bedKi 3.47
+  #define DEFAULT_bedKd 326.68
 #endif // PIDTEMPBED
 
 // @section extruder
